0c1b137fd39f3fd8f32ce11bc3cba6a2b16de818
justincaseof <justin.caseof@gmail.com>
2020-01-22 21:33:59 +0100

    ...tv time...

--- csvread.py
+++ csvread.py
@@ -11,6 +11,16 @@ i = 0
 multiline comment
 '''
 
+# deviation in percent to identify coherent voltages
+deviationpercentage = 20
+
+# temp variables
+base = 0
+sliceindex = 0
+
+slices=[]
+
+
 # print line-wise contents of csv file
 for line in csvarray.values:
     if i==0:
@@ -19,6 +29,13 @@ for line in csvarray.values:
         print("processing line %d" % i)
         voltage = float(line[2])
         print("  --> V=%f" % voltage)
+        if base*100/voltage > deviationpercentage:
+            print("!!! found beginning of new slice")
+            base = voltage
+            slices[sliceindex] = []
+            sliceindex += 1
+            # add current value
+            slices[sliceindex].append(voltage)
     i += 1
     
 
