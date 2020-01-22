cab94e65a53d3186effe99ceb2f21522b02ba80a
justincaseof <justin.caseof@gmail.com>
2020-01-22 21:27:21 +0100

    print voltages

--- csvread.py
+++ csvread.py
@@ -6,10 +6,24 @@ import collections
 csvarray = pd.read_csv("my.csv")
 i = 0
 
+# single line comment
+'''
+multiline comment
+'''
+
+# print line-wise contents of csv file
 for line in csvarray.values:
-    print("### LINE %d" % i)
-    i=i+1
-    for chunk in line:
-        print("-- %s --" % chunk)
+    if i==0:
+        print("skipping first line...")
+    else:
+        print("processing line %d" % i)
+        voltage = float(line[2])
+        print("  --> V=%f" % voltage)
+    i += 1
+    
+
+
+# let's see what pandas.cut() has in store for us
+
 
 
